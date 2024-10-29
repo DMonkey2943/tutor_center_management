bcf942bd93fcad9d0b3e7269eeea96f6655ab0d9
DMonkey2943 <ltnduyen29042003@gmail.com>
2024-10-29 23:16:35 +0700

    chore: update route to approve tutor's profile

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
     <nav class="nav flex-column mt-5">
         <a href="{{ route('admin.dashboard') }}" class="nav-link">Tổng Quan</a>
         <h6 class="text-uppercase font-weight-bold mt-3">Quản lý Gia sư</h6>
-        <a href="" class="nav-link">Xét duyệt hồ sơ Gia sư</a>
+        <a href="{{ route('admin.approvedTutors') }}" class="nav-link">Xét duyệt hồ sơ Gia sư</a>
         <a href="{{ route('admin.tutors') }}" class="nav-link">Danh sách Gia sư</a>
         <a href="" class="nav-link">Thêm Gia sư</a>
         <h6 class="text-uppercase font-weight-bold mt-3">Quản lý Phụ huynh</h6>
